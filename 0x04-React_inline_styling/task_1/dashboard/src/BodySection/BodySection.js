4fdcfb591b58adfdb343e03d89e764f32b2844b9
Henrietta Obioha <henriettaobioha1@gmail.com>
2024-02-13 08:22:30 -0800

    src

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './BodySection.css';
+import './BodySection.js';
 
 class BodySection extends Component {
   constructor(props) {
