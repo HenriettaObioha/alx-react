a84da5cfe0c42fbb8fa75219ca77caf2100bff4d
Henrietta Obioha <henriettaobioha1@gmail.com>
2024-02-13 08:52:37 -0800

    src

--- 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -1,30 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
-function NotificationItem({ type, html, value }) {
-  let li;
+class NotificationItem extends React.PureComponent {
+	constructor(props) {
+		super(props);
+	}
 
-  value
-    ? (li = <li data-notification-type={type}>{value}</li>)
-    : (li = (
-        <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
-      ));
-
-  return li;
+	render() {
+		const { type, html, value, markAsRead, id } = this.props;
+		const styleDataType = type === 'default' ? styles.blue : styles.red;
+		if (value) {
+			return (<li data-notification-type={type} className={css(styleDataType, styles.itemList)} onClick={() => markAsRead(id)} >{value}</li>);
+		} else {
+			return (<li dangerouslySetInnerHTML={html} className={css(styleDataType, styles.itemList)}onClick={() => markAsRead(id)} data-notification-type={type}></li>)
+		}
+	}
 }
 
-NotificationItem.defaultProps = {
-  type: 'default',
-  value: '',
-  html: {},
+const responsive = {
+	small: '@media screen and (max-width: 900px)'
 };
 
+const styles = StyleSheet.create({
+	red: {
+		color: 'red'
+	},
+
+	blue: {
+		color: 'blue'
+	},
+
+	itemList: {
+		[responsive.small]: {
+			display: 'block',
+			fontSize: '20px',
+			padding: '10px 8px',
+			listStyle: 'none',
+			borderBottom: '1px solid black'
+		}
+	}
+});
+
 NotificationItem.propTypes = {
-  type: PropTypes.string,
-  value: PropTypes.string,
-  html: PropTypes.shape({
-    __html: PropTypes.string,
-  }),
+	type: PropTypes.string,
+	value: PropTypes.string,
+	html: PropTypes.shape({
+		__html: PropTypes.string
+	}),
+	markAsRead: PropTypes.func,
+	id: PropTypes.number
+};
+
+NotificationItem.defaultProps = {
+	type: 'default',
+	value: '',
+	html: {},
+	markAsRead: () => {},
+	id: 0
 };
 
 export default NotificationItem;
